d63b769bd06c4d05a944d7b4009ae1347f0c1aa5
pedro emilio <potolino456@gmail.com>
2025-05-19 11:30:24 -0300

    avanco

--- AppTempoAgoraSQLite/App.xaml.cs
+++ AppTempoAgoraSQLite/App.xaml.cs
@@ -1,15 +1,43 @@
-﻿namespace AppTempoAgoraSQLite
+﻿using AppTempoAgoraSQLite.Models;
+using SQLite;
+
+
+namespace AppTempoAgoraSQLite
 {
     public partial class App : Application
     {
-        public App()
+        public class SQliteDatabaseHelper
         {
-            InitializeComponent();
-        }
+            readonly SQLiteAsyncConnection _conn;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            public SQliteDatabaseHelper(string path)
+            {
+                _conn = new SQLiteAsyncConnection(path);
+                _conn.CreateTableAsync<Tempo>().Wait();
+            }
+
+            public Task<int> Insert(Tempo p)
+            {
+                return _conn.InsertAsync(p);
+            }
+
+
+            public Task<int> Delete(int id)
+            {
+                return _conn.Table<Tempo>().DeleteAsync(i => i.Id == id);
+            }
+
+            public Task<List<Tempo>> GetAll()
+            {
+                return _conn.Table<Tempo>().ToListAsync();
+            }
+
+            public Task<List<Tempo>> Search(string q)
+            {
+                string sql = "SELECT * FROM Tempo WHERE descricao LIKE '%" + q + "%'";
+
+                return _conn.QueryAsync<Tempo>(sql);
+            }
         }
     }
 }
